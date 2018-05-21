2d4605fbc991f9a5d1391e899bfd2fb9d4415f0e
Карпачев Николай <Karpachev.N.K@sberbank.ru>
2018-05-21 17:17:04 +0300

    Добавил javadoc для SimplePhoto

--- app/src/main/java/com/example/foryaphoto/SimplePhoto.java
+++ app/src/main/java/com/example/foryaphoto/SimplePhoto.java
@@ -1,24 +1,39 @@
 package com.example.foryaphoto;
 
+/**
+ * Модель для хранения нескольких вариантов размера фотографии
+ *
+ * @author Karpachev Alexander
+ *         Created on 20.05.2018
+ */
 public class SimplePhoto {
+
     private String mSmallSize;
     private String mNormalSize;
     private String mOriginSize;
+
     public SimplePhoto(String smallSize){
         mSmallSize = smallSize;
         mNormalSize = smallSize;
         mOriginSize = smallSize;
     }
+
     public SimplePhoto(String smallSize, String normalSize){
         this(smallSize);
         mNormalSize = normalSize;
         mOriginSize = normalSize;
     }
+
     public SimplePhoto(String smallSize, String normalSize, String originSize){
         this(smallSize, normalSize);
         mOriginSize = originSize;
     }
 
+    /**
+     * Задать малый размер фото
+     *
+     * @param smallSize     размер фото
+     */
     public void setSmallSize(String smallSize){
         mSmallSize = smallSize;
     }
